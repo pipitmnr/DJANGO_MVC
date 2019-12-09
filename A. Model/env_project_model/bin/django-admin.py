8a2e22cc653ab7d733269b290b2f54ab1658a3f8
pipitmnr <syafitri@alterra.id>
2019-12-09 10:42:41 +0700

    env & project model

---
+++ A. Model/env_project_model/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/alta16/Documents/Purnamasm/Day16/DJANGO_MVC/A. Model/env_project_model/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
