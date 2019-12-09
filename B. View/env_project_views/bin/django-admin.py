a892ca55d69e62053841c3bfbc92d1c83bcf4108
pipitmnr <syafitri@alterra.id>
2019-12-09 11:01:13 +0700

    env project view & advance_view

---
+++ B. View/env_project_views/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/alta16/Documents/Purnamasm/Day16/DJANGO_MVC/B. View/env_project_views/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
